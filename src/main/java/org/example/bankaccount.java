8d1be8a56a84467b1e3a32470c0ce607a252e44e
bhuvaneshwari-k <123533635+bhuvaneshwari-k@users.noreply.github.com>
2023-01-30 15:36:43 +0530

    Update bankaccount.java

--- src/main/java/org/example/bankaccount.java
+++ src/main/java/org/example/bankaccount.java
@@ -1,5 +1,5 @@
 package org.example;
-
+import java.util.logging.Logger;
 import java.util.*;
 /**
  * Hello world!
@@ -7,6 +7,7 @@ import java.util.*;
  */
 class bankaccount
 {
+    static Logger l=Logger.getLogger("com.api.jar");
     String username;
     long number;
     double balance;
@@ -15,52 +16,52 @@ class bankaccount
         username = username1;
         number = num1;
         balance = balance1;
-        System.out.println("your name is:" + username);
-        System.out.println("your account number is:" + number);
-        System.out.println("your account is created!!!");
+        l.info("your name is:" + username);
+        l.info("your account number is:" + number);
+        l.info("your account is created!!!");
     }
 
     void deposit(long amount) {
         balance = balance + amount;
-        System.out.println("your amount is deposited successfully!!!");
+        l.info("your amount is deposited successfully!!!");
     }
 
     void withdrawal(long amount) {
         balance = balance - amount;
-        System.out.println("your amount is withdrawn successfully!!!");
+        l.info("your amount is withdrawn successfully!!!");
     }
 
     void displaybalance() {
-        System.out.println("your current balance is:" + balance);
+        l.info("your current balance is:" + balance);
     }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         Scanner s1 = new Scanner(System.in);
 
-        System.out.println("Enter username:");
+        l.info("Enter username:");
         String username1 = s1.nextLine();
-        System.out.println("Enter account number:");
+        l.info("Enter account number:");
         long num1 = s.nextLong();
         bankaccount ba = new bankaccount(username1, num1, 0.0);
         while (true) {
-            System.out.println("1.Deposit\n2.Withdrawal\n3.Current Balance\n4.Exit");
-            System.out.println("Enter your choice:");
+            l.info("1.Deposit\n2.Withdrawal\n3.Current Balance\n4.Exit");
+            l.info("Enter your choice:");
             int ch = s.nextInt();
             switch (ch) {
                 case 1 -> {
-                    System.out.println("Enter the amount to deposit:");
+                    l.info("Enter the amount to deposit:");
                     long amt = s.nextLong();
                     ba.deposit(amt);
                 }
                 case 2 -> {
-                    System.out.println("Enter the amount to withdraw:");
+                    l.info("Enter the amount to withdraw:");
                     long amt1 = s.nextLong();
                     ba.withdrawal(amt1);
                 }
                 case 3 -> ba.displaybalance();
                 default -> {
-                    System.out.println("you are exiting...!");
+                    l.info("you are exiting...!");
                     System.exit(0);
                 }
             }
